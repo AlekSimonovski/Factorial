8d4177f7faf7a2b0b5015e1e6b8616abd1de8cf4
Александар Симоновски <alek.simonovski02@gmail.com>
2023-04-02 21:34:10 +0200

    Added comments to code.

--- src/Faktoriel.java
+++ src/Faktoriel.java
@@ -2,12 +2,12 @@ import java.util.Scanner;
 public class Faktoriel {
 	public static void main(String[] args) {
         Scanner tastatura = new Scanner(System.in);
-        System.out.print("Vnesete ne-negativen integer: ");
+        System.out.print("Vnesete ne-negativen integer: "); // korisnicki vlez
         int n = tastatura.nextInt();
 
         long faktoriel = 1; // se koristi long za otpecatuvanje na rezultatot.
 
-        for (int i = 1; i <= n; i++) {
+        for (int i = 1; i <= n; i++) {	
             faktoriel *= i;
             if (faktoriel < 0) { // proveruva preliv
                 System.out.println("Prelevanje predizvikuva integerot n = " + n);
